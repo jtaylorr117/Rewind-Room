a56108ee7546fc296075035985c696ab19b4cf15
jtaylorr117 <jt10334n@pace.edu>
2025-04-14 19:57:22 -0400

    empty project

---
+++ Rewind Room/Rewind_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rewind_RoomApp.swift
+//  Rewind Room
+//
+//  Created by John Taylor on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Rewind_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rewind RoomTests/Rewind_RoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  Rewind_RoomTests.swift
+//  Rewind RoomTests
+//
+//  Created by John Taylor on 4/14/25.
+//
+
+import Testing
+@testable import Rewind_Room
+
+struct Rewind_RoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
